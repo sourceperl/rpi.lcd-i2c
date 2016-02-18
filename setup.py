1fef15986feb8f7117b061ac57dc78c7402a57e3
sourceperl <loic.celine@free.fr>
2016-02-18 16:38:52 +0100

    add setup.py for this module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='RPi_I2C_LCD',
+    version='0.1',
+    description='A simple LCD I2C driver on Rpi for Python',
+    author='Loic Lefebvre',
+    author_email="loic.celine@free.fr",
+    url="https://github.com/sourceperl/rpi.lcd-i2c",
+    py_modules=['RPi_I2C_LCD'],
+    platforms='any',
+    )
